d8c5c36b9e08e9c46795676a2f4a35cb203539ac
Andrew  Nam <andrewnam@Andrews-MacBook-Pro.local>
2018-06-24 21:55:57 -0700

    Complete App-2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      listOfCharacters: [
+        "Iron Man", 
+        "Batman", 
+        "Mega Man"
+      ]
+    } 
+  }
   render() {
+    let charactersToDisplay = this.state.listOfCharacters.map((e,i)=>{
+    return (
+      <h3 key={i}>{e}</h3>
+    )
+    })
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {charactersToDisplay}
       </div>
     );
   }
