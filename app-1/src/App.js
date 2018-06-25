43a48dfe15be44a264fcdc502279ab8cc1d9216b
Andrew  Nam <andrewnam@Andrews-MacBook-Pro.local>
2018-06-24 21:19:38 -0700

    Complete App1

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      blankLine: "",
+    }
+  }
+  onChangeHandle(event){
+    this.setState({
+      blankLine: event,
+    })
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={(event)=>this.onChangeHandle(event.target.value)}/> 
+        <p> {this.state.blankLine}</p>
       </div>
     );
   }
