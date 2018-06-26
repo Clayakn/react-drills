9e19ce259434bddf2bdaca4163e8bc8797bf884b
Andrew  Nam <andrewnam@Andrews-MacBook-Pro.local>
2018-06-25 19:56:27 -0700

    Finish App-5

---
+++ app-5/src/Image.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Image extends Component {
+  render() {
+    return (
+      <div>
+          <img src="https://http.cat/425" alt="Unordered Collection of Cats"/>
+      </div>
+    );
+  }
+}
+
+export default Image;
