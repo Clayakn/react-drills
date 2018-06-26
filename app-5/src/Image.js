0100f965c358959e481fe208c67dc09778fd7204
Andrew  Nam <andrewnam@Andrews-MacBook-Pro.local>
2018-06-25 20:00:38 -0700

    Change Image.js to functional component

--- app-5/src/Image.js
+++ app-5/src/Image.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 
-class Image extends Component {
-  render() {
+function Image (props)  {
     return (
       <div>
-          <img src="https://http.cat/425" alt="Unordered Collection of Cats"/>
+          <img src={props.myImage} alt="Unordered Collection of Cats"/>
       </div>
     );
   }
-}
 
 export default Image;
